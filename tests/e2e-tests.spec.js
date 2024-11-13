d4c9f741afe48ea13968131db54cb97460509d31
unknown <elyas.ablazelabs.io>
2024-11-13 15:47:36 +0300

    e2e test for navigating page

--- tests/e2e-tests.spec.js
+++ tests/e2e-tests.spec.js
@@ -3,11 +3,19 @@ const { test, describe, expect } = require("@playwright/test");
 describe("Pokedex", () => {
   test("front page can be opened", async ({ page }) => {
     await page.goto(""); // Provide the correct URL
-    await expect(page.getByText("Ivysaur")).toBeVisible();
+    await expect(page.getByText("ivysaur")).toBeVisible();
     await expect(
       page.getByText(
         "Pokémon and Pokémon character names are trademarks of Nintendo."
       )
     ).toBeVisible();
+  });
+
+  test("navigate to pokedex detail", async ({ page }) => {
+    await page.goto(""); // Replace with your actual URL
+
+    await page.click("text=Ivysaur");
+
+    await expect(page.getByText("chlorophyll")).toBeVisible(); //confirm if it navigates and find the text
   });
 });
