9111fe504abbef345344be13f53150052c304c82
unknown <elyas.ablazelabs.io>
2024-11-13 16:52:47 +0300

    prepare for render deploy in production

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-import express from "express";
+const express = require("express");
 const app = express();
+require("dotenv").config();
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
+
+app.get("/version", (req, res) => {
+  res.send("1"); // change this string to ensure a new version deployed
+});
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
