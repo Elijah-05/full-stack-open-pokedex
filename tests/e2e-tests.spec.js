7f03c1b8f93a6c285ef5c9ea4dfcdbde262976eb
unknown <elyas.ablazelabs.io>
2024-11-16 10:21:44 +0300

    version changed to 7 and webhook url added to github secret

--- tests/e2e-tests.spec.js
+++ tests/e2e-tests.spec.js
@@ -22,7 +22,7 @@ describe("Pokedex", () => {
   test("version route works fine", async ({ page }) => {
     await page.goto("version");
 
-    await expect(page.getByText("6")).toBeVisible();
+    await expect(page.getByText("7")).toBeVisible();
   });
 
   test("check the health is ok", async ({ page }) => {
